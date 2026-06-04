53c224343a696720e49e5eefd5c5c2fb69422ecc
Chloé-Célya Chulem-Chaya <chloecelya@hotmail.fr>
2026-06-04 11:39:29 +0200

    Google Maps API fait

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class ExploreScreen extends StatelessWidget {
   const ExploreScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    const LatLng surabaya = LatLng(
+      -7.2575,
+      112.7521,
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Explore'),
@@ -12,8 +18,21 @@ class ExploreScreen extends StatelessWidget {
         foregroundColor: Colors.black,
         elevation: 0,
       ),
-      body: const Center(
-        child: Text('Explore Screen tba'),
+      body: GoogleMap(
+        initialCameraPosition: const CameraPosition(
+          target: surabaya,
+          zoom: 12,
+        ),
+        markers: {
+          const Marker(
+            markerId: MarkerId('surabaya_marker'),
+            position: surabaya,
+            infoWindow: InfoWindow(
+              title: 'Surabaya',
+              snippet: 'Hidden Gems Test Location',
+            ),
+          ),
+        },
       ),
     );
   }
